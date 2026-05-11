1959a92ca26df7c7be8bc386240815cdaee6af4c
KingsLZ <w496830083@qq.com>
2026-05-11 17:16:48 +0800

    fix(di): Koin startKoin 补漏 androidContext() 配置

--- app/src/main/java/com/periodic/pro/App.kt
+++ app/src/main/java/com/periodic/pro/App.kt
@@ -2,12 +2,14 @@ package com.periodic.pro
 
 import android.app.Application
 import com.periodic.pro.di.appModule
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
+            androidContext(this@App)
             modules(appModule)
         }
     }
