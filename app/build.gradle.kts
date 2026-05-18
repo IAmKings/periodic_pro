46bcef3adb4a18d328686a18e2f5c8b61948bba3
KingsLZ <w496830083@qq.com>
2026-05-19 00:08:13 +0800

    版本号更新为 0.2.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.periodic.pro"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.2.7"
+        versionCode = 11
+        versionName = "0.2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
