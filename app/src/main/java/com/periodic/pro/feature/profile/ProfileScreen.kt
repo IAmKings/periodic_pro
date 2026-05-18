32cf5a00f0fad1db33bc86ef21c0c104d62ce49f
KingsLZ <w496830083@qq.com>
2026-05-19 01:13:51 +0800

    补充Box/background导入

--- app/src/main/java/com/periodic/pro/feature/profile/ProfileScreen.kt
+++ app/src/main/java/com/periodic/pro/feature/profile/ProfileScreen.kt
@@ -3,7 +3,9 @@ package com.periodic.pro.feature.profile
 import android.content.res.Configuration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
