7c3f62fc3f034eca993908197a4bf45ad406bef3
KingsLZ <w496830083@qq.com>
2026-05-20 18:05:03 +0800

    chore: 版本更新至 0.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.periodic.pro"
         minSdk = 24
         targetSdk = 35
-        versionCode = 17
-        versionName = "0.3.2"
+        versionCode = 18
+        versionName = "0.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
